ab7598b1949f605e03fc6d333a9116a47d6c78ce
Rodrigo Plotze <rodrigoplotze@gmail.com>
2025-02-25 09:12:49 -0300

    first

--- lib/controller/gorjeta_controller.dart
+++ lib/controller/gorjeta_controller.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class GorjetaController extends ChangeNotifier {
-  final double  _valorConta = 0.0;
-  final double _percentualGorjeta = 10.0;
-  final double _valorGorjeta = 0.0;
-  final double _totalPagar = 0.0;
+   double  _valorConta = 0.0;
+   double _percentualGorjeta = 10.0;
+   double _valorGorjeta = 0.0;
+   double _totalPagar = 0.0;
 
     List<double> listaGorjetas = [10.0, 15.0, 20.0, 25.0, 30.0];
 
